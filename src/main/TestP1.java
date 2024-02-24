dc913e130848f3af486d7ba55f918392a07a3b98
david <david@david>
2024-02-24 17:08:53 +0100

    Actualizando!

---
+++ src/main/TestP1.java
@@ -0,0 +1,16 @@
+
+package main;
+
+import irrgarten.Dice;
+import irrgarten.Directions;
+import irrgarten.GameCharacter;
+import irrgarten.GameState;
+import irrgarten.Orientation;
+import irrgarten.Shield;
+import irrgarten.Weapon;
+
+public class TestP1 {
+    public static void main (String[] args) {
+        System.out.println("Hola Mundo!");;
+    }
+}
